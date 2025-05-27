4ea919fa06fb01db43f2edbc4bf2fc0ffbcdc6f2
Shubham Kumar <shubhamhjp92@gmail.com>
2025-05-27 12:57:47 +0530

    changes in the vite.config.js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    historyApiFallback: true, // this enables SPA fallback for dev server
+  },
+  build: {
+    outDir: "dist",
+  },
+});
